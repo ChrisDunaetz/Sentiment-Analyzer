558f1d9fdc7673537384d4a0641c592169bc4357
Chris Dunaetz <chris@dunaetz.com>
2019-04-05 17:20:28 -0700

    Add vaderSentiment script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3
 ADD my_script.py /
-RUN pip install pystrich
+RUN pip install requests
 RUN pip install vaderSentiment
 CMD [ "python", "./my_script.py" ]
--- my_script.py
+++ my_script.py
@@ -1,7 +1,11 @@
-# Sample taken from pyStrich GitHub repository
-# https://github.com/mmulqueen/pyStrich
-from pystrich.datamatrix import DataMatrixEncoder
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-encoder = DataMatrixEncoder('This is a DataMatrix.')
-encoder.save('./datamatrix_test.png')
-print(encoder.get_ascii())
+analyser = SentimentIntensityAnalyzer()
+
+def scoresForString(sentence):
+  scores = analyser.polarity_scores(sentence)
+  return "{:-<60} {}".format(sentence, str(scores))
+
+  
+print(scoresForString("those people don't understand me, it makes me feel like something is wrong with me"))
+print(scoresForString("I really like Elaina, she's amazing!"))
